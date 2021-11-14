0bb82e91445955c6d8875029c6f70cc58e33ad8f
unknown <mahi121.mr@gmail.com>
2021-11-15 01:50:18 +0600

    review, post orders and delete operation added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require('mongodb');
+var ObjectId = require('mongodb').ObjectId;
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -12,39 +13,79 @@ app.use(express.json())
 const uri = `mongodb+srv://carMart:8vWlQdfxvoUwMcsl@cluster0.tecyb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run(){
-    try{
-        await client.connect()
-        console.log('Connected to Db')
+async function run() {
+  try {
+    await client.connect()
+    console.log('Connected to Db')
 
-        const database = client.db('car_mart')
-        const productCollection = database.collection('products')
-        const reviewCollection = database.collection('review')
-         
-        // GET PRODUCTS
-        app.get('/products', async(req,res)=>{
-            const cursor = productCollection.find({})
-            const products = await cursor.toArray()
-            console.log('hitted api')
-            res.send(products)
-        })
-        // GET Review
-        app.get('/review', async(req,res)=>{
-            const cursor = reviewCollection.find({})
-            const review = await cursor.toArray()
-            console.log('hitted api review')
-            res.send(review)
-        })
-        // Post Review
-        app.post('/review', async (req, res) => {
-          const review = req.body;
-          const result = await reviewCollection.insertOne(review)
-          res.json(result)
-      })
-    }
-    finally{
-        // await client.close()
-    }
+    const database = client.db('car_mart')
+    const productCollection = database.collection('products')
+    const reviewCollection = database.collection('review')
+    const orderCollection = database.collection('orders')
+
+    // GET PRODUCTS
+    app.get('/products', async (req, res) => {
+      const cursor = productCollection.find({})
+      const products = await cursor.toArray()
+      console.log('hitted api')
+      res.send(products)
+    })
+
+    // GET SPECIFIC PRODUCT
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id
+      console.log('getting id', id)
+      const query = { _id: ObjectId(id) }
+      const product = await productCollection.findOne(query)
+      res.json(product)
+    })
+
+    // GET Review
+    app.get('/review', async (req, res) => {
+      const cursor = reviewCollection.find({})
+      const review = await cursor.toArray()
+      console.log('hitted api review')
+      res.json(review) //not res.send
+    })
+
+    // Post Review
+    app.post('/review', async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review)
+      res.json(result)
+    })
+
+    // Get Orders
+    app.get('/orders', async (req, res) => {
+      const email = req.query.email
+      let query = {}
+      if (email) {
+        query = { email: email }
+      }
+      const cursor = orderCollection.find(query)
+      const orders = await cursor.toArray()
+      res.json(orders)
+    })
+
+    // Post Orders
+    app.post('/orders', async (req, res) => {
+      const order = req.body
+      const ans = await orderCollection.insertOne(order)
+      res.json(ans)
+    })
+
+    // Delete Orders
+    app.delete('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
+    })
+
+  }
+  finally {
+    // await client.close()
+  }
 }
 run().catch(console.dir)
 
