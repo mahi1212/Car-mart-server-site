6262083df3b6b91b4598e088b9ea64681b351759
unknown <mahi121.mr@gmail.com>
2021-11-15 21:55:39 +0600

    final touch done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://carMart:8vWlQdfxvoUwMcsl@cluster0.tecyb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tecyb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
@@ -121,7 +121,7 @@ async function run() {
       const result = await userCollection.updateOne(filter, updateDoc)
       res.json(result)
     })
-    
+
     // Check admin role
     app.get('/users/:email', async (req, res) => {
       const email = req.params.email;
